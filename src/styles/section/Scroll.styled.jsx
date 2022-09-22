26c8b458dfde06066f5828e6b110e7194c45ccda
Ritesh134340 <riteshkumar134340@gmail.com>
2022-09-22 19:54:29 +0530

    scroll fixed

--- src/styles/section/Scroll.styled.jsx
+++ src/styles/section/Scroll.styled.jsx
@@ -10,7 +10,7 @@ export const Button = styled.div`
   color: ${({ theme }) => theme.colors.orange};
   
   @media (max-width:${({theme})=>theme.mobile}){
-      left:50%;
+      left:47%;
       bottom:60px;
   }
 `;
